2f392a3096c71e1e75110cc1bed3eb781e88f617
Moritz Klein <moritz.klein@protonmail.com>
2019-05-30 17:58:42 +0200

    Try rasterizing image and feeding region to NN

--- ToolClassification/main.py
+++ ToolClassification/main.py
@@ -3,10 +3,25 @@ import load_dataset as data
 from keras.models import load_model
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+# DESIRED SIZE OF REGION
+X_SIZE = 64
+Y_SIZE = 64
 
 model = load_model('tool_model_80acc.h5')
-x = cv2.imread('dataset/split/test/hammer/0a98bc81e4dba8065f0cc9e6450cabdbc50f96a5.jpg')
-x = np.expand_dims(x, axis=0)
+image = cv2.imread('dataset/test_image.jpg')
 
-y_pred = model.predict(x)
-print(y_pred)
+y_dim, x_dim, _ = image.shape
+STEPSIZE = 50
+for x in range(0, x_dim-X_SIZE, STEPSIZE):
+    for y in range(0, y_dim-Y_SIZE, STEPSIZE):
+        region = image[y:y+64, x:x+64]
+        region = np.expand_dims(region, axis=0)
+        y_pred = model.predict(region)
+        print(y_pred)
+        plt.scatter(x, y, color=y_pred)
+
+    print("Rasterizing: {}%".format(x/(x_dim-X_SIZE)*100))
+plt.imshow(image)
+plt.show()
