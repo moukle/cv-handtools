91e5d98236adf87e7fec2429d516794715f43704
Moritz Klein <moritz.klein@protonmail.com>
2019-05-30 18:31:01 +0200

    Try different raster size and plot rects

--- ToolClassification/main.py
+++ ToolClassification/main.py
@@ -4,23 +4,33 @@ from keras.models import load_model
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 # DESIRED SIZE OF REGION
-X_SIZE = 64
-Y_SIZE = 64
+X_SIZE = 258
+Y_SIZE = 512
+
+X_SIZE_TARGET = 64
+Y_SIZE_TARGET = 64
 
 model = load_model('tool_model_80acc.h5')
 image = cv2.imread('dataset/test_image.jpg')
 
+
 y_dim, x_dim, _ = image.shape
-STEPSIZE = 50
+STEPSIZE = 64
 for x in range(0, x_dim-X_SIZE, STEPSIZE):
     for y in range(0, y_dim-Y_SIZE, STEPSIZE):
-        region = image[y:y+64, x:x+64]
+        region = image[y:y+Y_SIZE, x:x+Y_SIZE]
+        region = cv2.resize(region, (X_SIZE_TARGET, Y_SIZE_TARGET))
         region = np.expand_dims(region, axis=0)
+
+        # add alpha for plot
         y_pred = model.predict(region)
-        print(y_pred)
+        y_pred = np.append(y_pred, [1])
+
         plt.scatter(x, y, color=y_pred)
+        plt.gca().add_patch(Rectangle((x,y),X_SIZE,Y_SIZE,linewidth=2,edgecolor=y_pred,facecolor='none'))
 
     print("Rasterizing: {}%".format(x/(x_dim-X_SIZE)*100))
 plt.imshow(image)
