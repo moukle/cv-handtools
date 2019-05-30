5fc603f3a2bee23487125a21ac54c907d1ac2139
Moritz Klein <moritz.klein@protonmail.com>
2019-05-30 15:43:45 +0200

    Train model on 64x64 images

--- ToolClassification/crawl_data.sh
+++ ToolClassification/crawl_data.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
-folder_path="dataset/unsorted"
+folder_path=dataset
+folder_unsorted=$folder_path/unsorted
+folder_split=$folder_path/split
 
 # delete current dataset
 echo "Deleting current dataset"
-rm -rf $folder_path/*_images
+rm -rf $folder_unsorted $folder_split
 
+# SETTINGS
 scraper="pipenv run imagenetscraper"
-size="--size 128,128"
+size="--size 64,64"
 quiet=""
-# quiet="--quiet"
+quiet="--quiet"
 
-sysnet_id="n0000000"
+# sysnet_id="n0000000"
 # http://image-net.org/synset?wnid=n00000000
 #                                 ^^^^^^^^^
 #                                 SYNSET_ID
 
+#### DOWNLOAD IMAGES
 # hammer
 echo "Downloading hammer ..."
 sysnet_id="n03481172"
-$scraper $sysnet_id $folder_path/hammer_images $size $quiet
+$scraper $sysnet_id $folder_unsorted/hammer $size $quiet
 
 # wrench
 echo "Downloading wrench ..."
 sysnet_id="n02680754"
-$scraper $sysnet_id $folder_path/wrench_images $size $quiet
+$scraper $sysnet_id $folder_unsorted/wrench $size $quiet
 
 # plane
 echo "Downloading plane ..."
 sysnet_id="n03954731"
-$scraper $sysnet_id $folder_path/plane_images $size $quiet
+$scraper $sysnet_id $folder_unsorted/plane $size $quiet
+
+
+#### SPLIT IN TRAIN / VAL / TEST
+echo "Splitting data in train/val/test dataset ..."
+pipenv run split_folders $folder_unsorted --output $folder_split --ratio .8 .1 .1
--- ToolClassification/load_dataset.py
+++ ToolClassification/load_dataset.py
@@ -1,6 +1,30 @@
 import os
-import split_folders
+import pathlib
+import random
+import cv2
+import numpy as np
 
-IN_PATH = "dataset/unsorted"
-OUT_PATH = "dataset/split"
-split_folders.ratio(input=IN_PATH, output=OUT_PATH, seed=1337, ratio=(.8, .1, .1))
+data_path = "dataset/split/"
+
+def load_x_y(data_path):
+    data_root = pathlib.Path(data_path)
+
+    all_image_paths = list(data_root.glob('*/*'))
+    all_image_paths = [str(path) for path in all_image_paths]
+    random.shuffle(all_image_paths)
+
+    label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+    label_to_index = dict((name, index) for index,name in enumerate(label_names))
+    all_image_labels = [label_to_index[pathlib.Path(path).parent.name]
+                    for path in all_image_paths]
+
+    all_images = [cv2.imread(file) for file in all_image_paths]
+
+    return all_images, all_image_labels
+
+def training():
+    return load_x_y(data_path+"train")
+def validation():
+    return load_x_y(data_path+"val")
+def test():
+    return load_x_y(data_path+"test")
