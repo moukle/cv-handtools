7d0ec26e664534b87187487a24d998bdb8a3ad33
Moritz Klein <moritz.klein@protonmail.com>
2019-06-22 11:47:31 +0200

    Make rasterize working again
    Former-commit-id: a847850957aa9ae46833e4b8e5c4724a8888c796

--- ToolClassification/rasterize_image.py
+++ ToolClassification/rasterize_image.py
@@ -1,27 +1,24 @@
+# ==================================================
 # DEPRECATED
+# ==================================================
 
-
-import load_dataset as data
-
-from keras.models import load_model
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from keras.models import load_model
 
 # LOAD DATA
 # model = load_model('tool_model_80acc.h5')
 model = load_model('tool_model.h5')
 image = cv2.imread('dataset/test_image5.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
 toolPositions = {}
 toolLabels = {
     0: "background",
     1: "hammer",
-    # 1: "plane",
-    2: "wrench"
+    2: "plane",
+    3: "wrench"
 }
 
 # SELECTED REGION SIZE
@@ -31,8 +28,6 @@ Y_SIZE = 256*2
 # SIZE REQUIRED BY THE NETWORK
 X_SIZE_TARGET = 224
 Y_SIZE_TARGET = 224
-X_SIZE_TARGET = 64 *2
-Y_SIZE_TARGET = 64 *2
 
 # ACTUAL SHAPE OF THE IMAGE
 y_dim, x_dim, _ = image.shape
@@ -45,18 +40,16 @@ for y in range(0, y_dim-Y_SIZE, STEPSIZE):
         region = cv2.resize(region, (X_SIZE_TARGET, Y_SIZE_TARGET))
         region = np.expand_dims(region, axis=0)
 
-        # add alpha for plot [RGBA]
         y_pred = model.predict(region)
         print(y_pred)
 
         if np.argmax(y_pred) != 0 and np.max(y_pred) > 0.9:
             rect = (y,y+Y_SIZE, x,x+X_SIZE)
             toolPositions[rect] = toolLabels[np.argmax(y_pred)]
-            y_pred = np.append(y_pred, [1])
-            plt.scatter(x, y, s=100, color=y_pred)
-            plt.gca().add_patch(Rectangle((x,y),X_SIZE,Y_SIZE,linewidth=2,edgecolor=y_pred,facecolor='none'))
+            y_pred = (y_pred[0][1:])
+            plt.scatter(x, y, s=100, color=y_pred) # dot
+            plt.gca().add_patch(Rectangle((x,y),X_SIZE,Y_SIZE,linewidth=2,edgecolor=y_pred,facecolor='none')) # rect
 
     print("Rasterizing: {}%".format(y/(y_dim-Y_SIZE)*100))
-# print(toolPositions)
 plt.imshow(image)
 plt.show()
